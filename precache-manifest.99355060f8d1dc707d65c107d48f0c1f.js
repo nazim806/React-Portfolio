5068f19622a42306c52fe948ad8d7d13732e3576
Nazim Uddin <umnazim@gmail.com>
2020-11-02 20:42:27 -0800

    Updates

---
+++ precache-manifest.99355060f8d1dc707d65c107d48f0c1f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce571d87b1c955bed2407de061344cce",
+    "url": "/React-Portfolio/index.html"
+  },
+  {
+    "revision": "6958ddde169c640394a3",
+    "url": "/React-Portfolio/static/css/2.559fd73f.chunk.css"
+  },
+  {
+    "revision": "dae173b10d045cd2c048",
+    "url": "/React-Portfolio/static/css/main.a22aecf2.chunk.css"
+  },
+  {
+    "revision": "6958ddde169c640394a3",
+    "url": "/React-Portfolio/static/js/2.a3c74682.chunk.js"
+  },
+  {
+    "revision": "dae173b10d045cd2c048",
+    "url": "/React-Portfolio/static/js/main.e56f071c.chunk.js"
+  },
+  {
+    "revision": "755f3015286e7cb72a72",
+    "url": "/React-Portfolio/static/js/runtime-main.14d89931.js"
+  }
+]);
